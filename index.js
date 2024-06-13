1ccc1b81a7a0ff21d69aad1cfaf586f43f59177f
Ilyeasss <ilyasbouelbaraka@gmail.com>
2024-06-13 14:47:37 +0100

    form added

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-document.querySelector(".contact-btn").addEventListener("click", function(){
-  
-})
+document.querySelector(".contact-btn").addEventListener("click", function() {
+  document.querySelector(".contact-form-container").classList.add("active");
+  document.querySelector("#overlay").classList.add("active");
+});
+
+document.querySelector("#overlay").addEventListener("click", function() {
+  document.querySelector(".contact-form-container").classList.remove("active");
+  document.querySelector("#overlay").classList.remove("active");
+});
+
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+  let formIsValid = true;
+  const formGroups = document.querySelectorAll(".form-group");
+
+  formGroups.forEach(group => {
+      const input = group.querySelector("input, textarea");
+      const errorMessage = group.querySelector(".error-message");
+
+      if (!input.value.trim()) {
+          errorMessage.textContent = `${input.placeholder} peux pas etre vide`;
+          errorMessage.style.display = "block";
+          formIsValid = false;
+      } else {
+          errorMessage.style.display = "none";
+      }
+  });
+
+  if (formIsValid) {
+      alert("Form submitted successfully!");
+  }
+});
